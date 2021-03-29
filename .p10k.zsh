7637e99daeb65aad608bb631acc5f5eb1da040e4
Klas Mellbourn <klas@mellbourn.net>
2021-03-29 21:19:31 +0200

    powerlevel10k: activate rvm again

--- .p10k.zsh
+++ .p10k.zsh
@@ -67,7 +67,7 @@
     # java_version          # java version (https://www.java.com/)
     # package               # name@version from package.json (https://docs.npmjs.com/files/package.json)
     rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
-    # rvm                     # ruby version from rvm (https://rvm.io)
+    rvm                     # ruby version from rvm (https://rvm.io)
     fvm                     # flutter version management (https://github.com/leoafarias/fvm)
     luaenv                  # lua version from luaenv (https://github.com/cehoffman/luaenv)
     jenv                    # java version from jenv (https://github.com/jenv/jenv)
