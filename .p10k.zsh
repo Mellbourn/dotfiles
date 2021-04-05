26c9c73d3662ec6e7bca003da0171748032ea01c
Klas Mellbourn <klas@mellbourn.net>
2021-04-05 23:34:40 +0200

    powerlevel10k: make dir shortening clearer

--- .p10k.zsh
+++ .p10k.zsh
@@ -225,12 +225,12 @@
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=237
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=238
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=17
   # Display anchor directory segments in bold.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
   # Don't shorten directories that contain any of these files. They are anchors.
   local anchor_files=(
     .bzr
