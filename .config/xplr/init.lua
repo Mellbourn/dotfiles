e4a4a903958710f863bdb92c80d009d907fa34c1
Klas Mellbourn <klas@mellbourn.net>
2022-10-09 22:26:16 +0200

    install xplr with some plugins

---
+++ .config/xplr/init.lua
@@ -0,0 +1,12 @@
+version = "0.19.4"
+
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+
+require"icons".setup()
+require"map".setup()
+require"zoxide".setup()
