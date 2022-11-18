c635868c26027d776a6c65acaf090adea2a74290
Klas Mellbourn <klas@mellbourn.net>
2022-11-19 00:00:12 +0100

    cleanup-branches.mjs: improved script

--- bin/cleanup-branches.mjs
+++ bin/cleanup-branches.mjs
@@ -2,4 +2,23 @@
 
 const neverDelete = "master\\|main\\|develop\\|hotfix\\|temp\\|[0-9]task";
 
-await $`git branch --merged | grep  -v ${neverDelete} | xargs -n 1 git branch -d`;
+try {
+  await $`git branch --merged | grep  -v ${neverDelete} | xargs -n 1 git branch -d`;
+} catch (p) {
+  console.log(`No merged branches to delete (${p.exitCode})`);
+}
+
+let branches = [];
+
+try {
+  branches = await $`git branch --no-merged | grep  -v ${neverDelete}`;
+} catch (p) {
+  console.log(`No unmerged branches to delete (${p.exitCode})`);
+  process.exit(0);
+}
+
+console.log(chalk.blue(`Branches to delete: ${branches.stdout}`));
+
+// branches.forEach((branch) => {
+//   console.log(`delete ${branch}`);
+// });
