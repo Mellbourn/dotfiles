0087844f18253644ca61865c758c22e52f07f236
Klas Mellbourn <klas@mellbourn.net>
2022-11-18 23:34:10 +0100

    remove weird wildcard

--- bin/cleanup-branches.mjs
+++ bin/cleanup-branches.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env zx
 
-const neverDelete = "\\*\\|master\\|main\\|develop\\|hotfix\\|temp\\|[0-9]task";
+const neverDelete = "master\\|main\\|develop\\|hotfix\\|temp\\|[0-9]task";
 
 await $`git branch --merged | grep  -v ${neverDelete} | xargs -n 1 git branch -d`;
