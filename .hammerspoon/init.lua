23fa72e070816933bac6b1fe6d510d3c94993ca0
Klas Mellbourn <klas@mellbourn.net>
2024-01-14 22:01:48 +0100

    only resize screens if I'm on Klas’s MacBook Pro 16" 2023

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,14 @@
 -- make sure displays are placed correctly on wakeup
 function printf(s,...)  print(s:format(...)) end
-wather = hs.caffeinate.watcher.new(function(eventType)
-    -- screensDidWake, systemDidWake, screensDidUnlock
-    if eventType == hs.caffeinate.watcher.screensDidUnlock then
-        local output = hs.execute("/Users/klas.mellbourn/bin/dp", false)
-        -- hs.notify.new({title="TestTitle", informativeText=output}):send()
-        -- printf("%s, world", output)
-    end
-end)
-wather:start()
+local machineName = hs.host.localizedName()
+if machineName == 'Klas’s MacBook Pro 16" 2023' then
+    watcher = hs.caffeinate.watcher.new(function(eventType)
+        -- screensDidWake, systemDidWake, screensDidUnlock
+        if eventType == hs.caffeinate.watcher.screensDidUnlock then
+            local output = hs.execute("/Users/klas.mellbourn/bin/dp", false)
+            -- hs.notify.new({title="TestTitle", informativeText=output}):send()
+            -- printf("%s, world", output)
+        end
+    end)
+    watcher:start()
+end
